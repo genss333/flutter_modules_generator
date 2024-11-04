0388ae73a26442a3f42e099fcd0bef16fe2da00e
Tusspon Moonsan <109135068+genss333@users.noreply.github.com>
2024-11-04 17:02:45 +0700

    Refactor import

--- bin/flutter_modules_generator.dart
+++ bin/flutter_modules_generator.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import '../lib/flutter_modules_generator.dart';
+import 'package:flutter_modules_generator/flutter_modules_generator.dart';
 
 void main(List<String> arguments) {
   stdout.write('Enter the module name: ');
